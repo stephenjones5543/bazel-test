211b32f2d4d35e153a4fc58bfdcee9210103c9d2
Adedapo Ajuwon <dapseen@gmail.com>
2021-04-26 23:48:48 +0100

    initial commit

---
+++ main/main.py
@@ -0,0 +1,8 @@
+import numpy
+
+#print("This Works")
+print(numpy.datetime64('2005-02-25'))
+#copy all the req from lib/interceptor
+#copy all req to build file try that
+#take main file and build file.. move it to aiig project and try run it 
+#
